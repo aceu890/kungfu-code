e6769049cc8765d4fd4aae4ab8fc2b03f7a5ca54
Fernando <eldersoto1876@hotmail.com>
2022-12-20 20:35:24 -0300

    add cards info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+//LOADER 
+window.addEventListener('load', () => {
+	const contenedor_loader = document.querySelector('.contenedor_loader')
+	contenedor_loader.style.opacity = 0
+	contenedor_loader.style.visibility = 'hidden'
+})
+
+// Activar y desactivar Menu
 addEventListener('DOMContentLoaded', () => {
     const btn_menu = document.querySelector('.btn_menu')
     if (btn_menu) {
@@ -7,3 +15,5 @@ addEventListener('DOMContentLoaded', () => {
         })
     }
 })
+
+
